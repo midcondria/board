3c8fe891b304699ea5dc5ebd39f6ca382ea560ce
midcondria <hyukkind@naver.com>
2023-02-05 09:48:01 +0900

    커밋13

--- src/main/java/my/board/web/login/LoginController.java
+++ src/main/java/my/board/web/login/LoginController.java
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletResponse;
 
-@Controller
+@RestController
 @Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/users")
